347428121d64df7d9b08fcabf1c1245a192935b3
Aaron Griffith <aargri@gmail.com>
2026-04-16 18:34:59 -0400

    add helpful pattern matcher methods to Event and Value

--- src/types.rs
+++ src/types.rs
@@ -10,6 +10,36 @@ pub enum Value {
     Float(Float),
 }
 
+impl Value {
+    pub fn as_null(&self) -> Option<()> {
+        matches!(self, Self::Null).then_some(())
+    }
+
+    pub fn as_bool(&self) -> Option<bool> {
+        if let Self::Bool(v) = self {
+            Some(*v)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_integer(&self) -> Option<Integer> {
+        if let Self::Integer(v) = self {
+            Some(*v)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_float(&self) -> Option<Float> {
+        if let Self::Float(v) = self {
+            Some(*v)
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Event<'de> {
@@ -21,4 +51,50 @@ pub enum Event<'de> {
     EnumClose,
     Key(&'de str),
     Value(Value),
+}
+
+impl<'de> Event<'de> {
+    pub fn as_list_open(&self) -> Option<()> {
+        matches!(self, Self::ListOpen).then_some(())
+    }
+
+    pub fn as_list_close(&self) -> Option<()> {
+        matches!(self, Self::ListClose).then_some(())
+    }
+
+    pub fn as_map_open(&self) -> Option<()> {
+        matches!(self, Self::MapOpen).then_some(())
+    }
+
+    pub fn as_map_close(&self) -> Option<()> {
+        matches!(self, Self::MapClose).then_some(())
+    }
+
+    pub fn as_enum_open(&self) -> Option<&'de str> {
+        if let Self::EnumOpen(name) = self {
+            Some(name)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_enum_close(&self) -> Option<()> {
+        matches!(self, Self::EnumClose).then_some(())
+    }
+
+    pub fn as_key(&self) -> Option<&'de str> {
+        if let Self::Key(name) = self {
+            Some(name)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_value(&self) -> Option<&Value> {
+        if let Self::Value(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
 }
