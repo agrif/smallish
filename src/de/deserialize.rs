b781a0f51b12cb50f4e31b363024ab8cd7fe9647
Aaron Griffith <aargri@gmail.com>
2026-04-19 18:49:01 -0400

    more correctly handle any and newtype_struct inside EscapedAccess

--- src/de/deserialize.rs
+++ src/de/deserialize.rs
@@ -811,9 +811,31 @@ where
     type Error = Error;
 
     forward_to_inner_deserialize! {
-        any bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char
-        option unit unit_struct newtype_struct seq tuple
+        bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char
+        option unit unit_struct seq tuple
         tuple_struct map struct enum identifier ignored_any
+    }
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        match self.de.peek()? {
+            Event::Value(Value::String(_)) => self.deserialize_str(visitor),
+            Event::Value(Value::Bytes(_)) => self.deserialize_bytes(visitor),
+            _ => self.de.deserialize_any(visitor),
+        }
+    }
+
+    fn deserialize_newtype_struct<V>(
+        self,
+        _name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: de::Visitor<'de>,
+    {
+        visitor.visit_newtype_struct(self)
     }
 
     fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
