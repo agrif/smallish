e3f80bd840b5ad42ea9df81be5873cef84a6b8df
Aaron Griffith <aargri@gmail.com>
2026-04-20 21:53:48 -0400

    add panic and assert macros to conditionally route to defmt

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,3 +1,143 @@
+#![macro_use]
+
+#[macro_use]
+#[allow(unused)]
+mod macros {
+    // compile-time dispatch to defmt (or not!)
+    // (adapted and mostly copied from embassy)
+
+    // We check both feature = "defmt" and target_os = "none" so that
+    // examples and tests can still compile and link on std.
+    // If you are here because you are running defmt on std, I'm sorry.
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! assert {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::assert!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::assert!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! assert_eq {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::assert_eq!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::assert_eq!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! assert_ne {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::assert_ne!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::assert_ne!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug_assert {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::debug_assert!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::debug_assert!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug_assert_eq {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::debug_assert_eq!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::debug_assert_eq!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug_assert_ne {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::debug_assert_ne!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::debug_assert_ne!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! todo {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::todo!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::todo!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! unreachable {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::unreachable!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::unreachable!($($x)*);
+            }
+        };
+    }
+
+    #[collapse_debuginfo(yes)]
+    macro_rules! panic {
+        ($($x:tt)*) => {
+            {
+                #[cfg(not(all(feature = "defmt", target_os = "none")))]
+                ::core::panic!($($x)*);
+                #[cfg(all(feature = "defmt", target_os = "none"))]
+                ::defmt::panic!($($x)*);
+            }
+        };
+    }
+
+    #[cfg(all(feature = "defmt", target_os = "none"))]
+    #[collapse_debuginfo(yes)]
+    macro_rules! unwrap {
+        ($($x:tt)*) => {
+            ::defmt::unwrap!($($x)*)
+        };
+    }
+
+    #[cfg(not(all(feature = "defmt", target_os = "none")))]
+    #[collapse_debuginfo(yes)]
+    macro_rules! unwrap {
+        ($arg:expr) => {
+            $arg.unwrap()
+        };
+        ($arg:expr, $msg:expr) => {
+            $arg.expect($msg)
+        };
+    }
+}
+
 // helper to format strings nicely in error message
 pub struct FormatIter<'a, I> {
     iter: core::cell::Cell<Option<I>>,
